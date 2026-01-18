570217a761f17a9be6525b898b1e60bcd6b9dabe
muhammed-alateeqi1 <mu.alateeqi@gmail.com>
2026-01-18 21:11:35 +0200

    create structure components

---
+++ src/app/core/lead.model.ts.ts
@@ -0,0 +1,2 @@
+export interface LeadModelTs {
+}
---
+++ src/app/pages/leads/leads.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leads',
+  standalone: true,
+  imports: [],
+  templateUrl: './leads.component.html',
+  styleUrl: './leads.component.css'
+})
+export class LeadsComponent {
+
+}
