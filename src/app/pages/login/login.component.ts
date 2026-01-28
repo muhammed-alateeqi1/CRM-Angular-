37397c38902fa805637b63ecae6200f33c202bd3
muhammed-alateeqi1 <mu.alateeqi@gmail.com>
2026-01-28 02:57:24 +0200

    feat: complete login form with validation, auth errors and spinner

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core'
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -9,50 +9,50 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
     styleUrl: './login.component.css',
 })
 export class LoginComponent {
-    emailScheme: string = 'admin@crm.com'
-    passwordScheme: string = '#admin123'
-    authError: string = ''
-    isLoading: boolean = false
+    emailScheme: string = 'admin@crm.com';
+    passwordScheme: string = '#admin123';
+    authError: string = '';
+    isLoading: boolean = false;
     constructor() {}
     ngOnInit() {
-        this.clearAuthErrorOnInputChange()
+        this.clearAuthErrorOnInputChange();
     }
     loginForm = new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    })
+        email: new FormControl<string>('', [Validators.required, Validators.email]),
+        password: new FormControl<string>('', [Validators.required, Validators.minLength(8)]),
+    });
 
     formValidation() {
-        this.authError = ''
+        this.authError = '';
         if (this.loginForm.invalid) {
-            this.loginForm.markAllAsTouched()
-            return
+            this.loginForm.markAllAsTouched();
+            return;
         }
         //auth logic
-        this.isLoading = true
-        setTimeout(()=>{
-          const { email, password } = this.loginForm.value
-          console.log(this.loginForm.value)
-  
-          if (email !== this.emailScheme) {
-              this.authError = 'incorrect email'
-              console.log('incorrect email')
-              this.isLoading = false
-              return
-          }
-          if (password !== this.passwordScheme) {
-              this.authError = 'incorrect password try to enter vaild password'
-              console.log('incorrect password')
-              this.isLoading = false
-              return
-          }
-          console.log('Login Successful <3')
-          this.isLoading = false;
-        },1000)
+        this.isLoading = true;
+        setTimeout(() => {
+            const email = this.loginForm.value.email?.trim();
+            const password = this.loginForm.value.password;
+            // console.log(this.loginForm.value)
+            if (email !== this.emailScheme) {
+                this.authError = 'incorrect email';
+                console.log('incorrect email');
+                this.isLoading = false;
+                return;
+            }
+            if (password !== this.passwordScheme) {
+                this.authError = 'incorrect password try to enter vaild password';
+                console.log('incorrect password');
+                this.isLoading = false;
+                return;
+            }
+            console.log('Login Successful <3');
+            this.isLoading = false;
+        }, 1000);
     }
     clearAuthErrorOnInputChange() {
         this.loginForm.valueChanges.subscribe(() => {
-            this.authError = ''
-        })
+            this.authError = '';
+        });
     }
 }
